49f1db6aec91eb7c3605a0446ba6217792643d03
simonbeelen <74742179+simonbeelen@users.noreply.github.com>
2025-06-05 20:19:58 +0200

    aanmaak templates (header, footer en layout)

---
+++ programingproject/src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import react from 'react';
+import Header from './Header';  
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Header />
+      <main className="main-content">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;  
