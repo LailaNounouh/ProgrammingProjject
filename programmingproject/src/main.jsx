9ed6b53b3e5c1186103f4d8ddae8e41cf6956417
simonbeelen <74742179+simonbeelen@users.noreply.github.com>
2025-06-05 13:41:37 +0200

    Delete programmingproject directory
    veel problemen met een slechte vite installatie dus we gaan herbeginnen met het installeren van vite

--- programmingproject/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import LoginPagina from "./paginas/LoginPagina";
-import "./style.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<LoginPagina />);
