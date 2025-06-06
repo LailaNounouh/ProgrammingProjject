aafc36fb3cd3b134fccc6ac7ae6eed92c9451463
simonbeelen <74742179+simonbeelen@users.noreply.github.com>
2025-06-06 11:44:20 +0200

    aanpassing header

--- programingproject/src/components/layout/Header.jsx
+++ programingproject/src/components/layout/Header.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo from '../../public/erasmus-logo.png'; 
 
 const Header = () => {
   return (
     <header className="header">
+      <img src={logo} alt="Erasmus logo" className="logo" />
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/login">Login</Link></li>
         </ul>
       </nav>
     </header>
