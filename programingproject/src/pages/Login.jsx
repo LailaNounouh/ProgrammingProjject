244114c105a87f5579edfdebd7ad6817ea8fdf8f
simonbeelen <74742179+simonbeelen@users.noreply.github.com>
2025-06-06 09:24:13 +0200

    aanpassing van de dubelle header

--- programingproject/src/pages/Login.jsx
+++ programingproject/src/pages/Login.jsx
@@ -1,11 +1,9 @@
 import React from "react";  
-import Header from "../components/layout/Header";
 import "./Login.css"; 
 
 export default function Login() {
   return (
     <div className="login">
-      <Header />
       <div className="login-content">
         <h1>Inloggen als:</h1>
         <div className="login-options">
--- programingproject/src/pages/Register.jsx
+++ programingproject/src/pages/Register.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "../components/layout/Header";
 import "./Register.css";
 
 export default function Register() {
   return (
     <div className="register">
-      <Header />
       <div className="register-content">
         <h1>Registreren</h1>
         <p>Vul het onderstaande formulier in om je te registreren.</p>
