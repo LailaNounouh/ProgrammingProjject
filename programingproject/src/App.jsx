b26ce10ffab6e081b12e0e7b7a72e3752d1e0977
simonbeelen <74742179+simonbeelen@users.noreply.github.com>
2025-06-10 15:41:14 +0200

    toevoeging dfault host

--- programingproject/src/App.jsx
+++ programingproject/src/App.jsx
@@ -17,6 +17,10 @@ import Contact from "./pages/Algemeen/Contact";
 
 
 import StudentDashboard from "./pages/Student/StudentenDashboard";
+import Afspraken from "./pages/Student/AfsprakenModule";
+import Standen from "./pages/Student/StandenModule";
+import Bedrijven from "./pages/Student/BedrijvenModule";
+
 // import BedrijfDashboard from "./pages/Bedrijven/BedrijvenDashboard";
 
 
@@ -31,9 +35,9 @@ export default function App() {
           <Route path="/contact" element={<Layout><Contact /></Layout>} />
 
           <Route path="/student" element={<StudentenLayout><StudentDashboard /></StudentenLayout>} />
-          <Route path="/student/bedrijven" element={<StudentenLayout><BedrijvenDashboard /></StudentenLayout>} />
-          <Route path="/student/standen" element={<StudentenLayout><StandenOverzicht /></StudentenLayout>} />
-          <Route path="/student/afspraak" element={<StudentenLayout><AfspraakFormulier /></StudentenLayout>} />
+          <Route path="/student/bedrijven" element={<StudentenLayout><Bedrijven /></StudentenLayout>} />
+          <Route path="/student/standen" element={<StudentenLayout><Standen /></StudentenLayout>} />
+          <Route path="/student/afspraak" element={<StudentenLayout><Afspraken /></StudentenLayout>} />
 
           <Route path="*" element={<Notfound />} />
         </Routes>
---
+++ programingproject/src/pages/Student/BedrijvenModule.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Bedrijven() {
+  return (
+    <div className="page-container">
+      <h2>Deelnemende Bedrijven</h2>
+      <p>Hier komt een overzicht van alle bedrijven die deelnemen aan de beursdag.</p>
+      <ul>
+        <li>Microsoft</li>
+        <li>Webdoos</li>
+        <li>Sopra Steria</li>
+        <li>Cisco</li>
+      </ul>
+    </div>
+  );
+}
