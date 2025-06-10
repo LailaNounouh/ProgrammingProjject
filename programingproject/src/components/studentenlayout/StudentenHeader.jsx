aff99e1284e5f520e28147df3ca2a2f20b8a453d
simonbeelen <74742179+simonbeelen@users.noreply.github.com>
2025-06-10 14:06:49 +0200

    toevoeging logout button

--- programingproject/src/components/studentenlayout/StudentenHeader.jsx
+++ programingproject/src/components/studentenlayout/StudentenHeader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LogoutButton from '../button/logoutbutton.jsx';
 import './StudentenHeader.css';
 
 const StudentenHeader = () => {
@@ -15,9 +16,9 @@ const StudentenHeader = () => {
           <li><Link to="/student/bedrijven">Deelnemende Bedrijven</Link></li>
           <li><Link to="/student/standen">Standen</Link></li>
           <li><Link to="/student/afspraak">Afspraak maken</Link></li>
-          <li><Link to="/student/logout">Logout</Link></li>
         </ul>
       </nav>
+      <LogoutButton />
     </header>
   );
 };
