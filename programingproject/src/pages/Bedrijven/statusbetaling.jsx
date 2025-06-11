3bb81c3ba1244dc071dc18b354b911dfc80e7ce4
LailaNounouh <lailanounouh@gmail.com>
2025-06-11 15:37:51 +0200

    statusbetaling css en jsx
    basis layout en styling

---
+++ programingproject/src/pages/Bedrijven/statusbetaling.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './statusbetaling.css';
+
+const StatusBetaling = () => {
+  return (
+    <div className="payment-status-page">
+      <div className="payment-status">
+        {/* Hier komen straks de payment cards */}
+      </div>
+    </div>
+  );
+};
+
+export default StatusBetaling;
