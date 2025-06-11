56bb727b9b7118f3ebb911666f59431abd2de0a0
Bjorn-guiot <bjorn.guiot@student.ehb.be>
2025-06-11 09:27:53 +0200

    update

--- programingproject/src/pages/Algemeen/login/Login.jsx
+++ programingproject/src/pages/Algemeen/login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
-import { baseUrl } from "../../../config"; //
+import { baseUrl } from "../../../config";
 
 export default function Login() {
   const navigate = useNavigate();
