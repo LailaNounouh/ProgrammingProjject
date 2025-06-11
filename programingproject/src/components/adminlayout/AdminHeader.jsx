75b6b237e8785280c660037ee6cc47aa66d5f36d
nawfel-amechrouk <146715536+nawfel-amechrouk@users.noreply.github.com>
2025-06-11 10:42:41 +0200

    Nieuwe AdminHeader gemaakt met logo en navigatie
    - Oude AdminHeader vervangen door een nieuwe versie
    - Erasmus logo, admin titel en navigatie toegevoegd
    - Navigatie bevat links naar bedrijven, standen, gebruikers en statistieken
    - Hamburger icoon toegevoegd rechtsboven

--- programingproject/src/components/adminlayout/AdminHeader.jsx
+++ programingproject/src/components/adminlayout/AdminHeader.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import './AdminHeader.css';
 
-export default function AdminHeader() {
-  return (
-    <header className="admin-header">
-      <h2>Admin</h2>
-      <nav>
-        <ul>
-          <li><Link to="/admin/dashboard">Dashboard</Link></li>
-          <li><Link to="/admin/users">Gebruikers</Link></li>
-          <li><Link to="/admin/settings">Instellingen</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+const AdminHeader = () => {
+return (
+<header className="header">
+<Link to="/" className="logo-link">
+<div className="logo">
+erasmus<br />
+<small>Hogeschool Brussel</small>
+</div>
+</Link>
+
+<h1 className="admin-title">Admin</h1>
+
+<nav>
+<ul>
+<li><Link to="/admin/bedrijven">Deelnemende bedrijven</Link></li>
+<li><Link to="/admin/standen">Beheer van standen</Link></li>
+<li><Link to="/admin/users">Beheer van gebruikers</Link></li>
+<li><Link to="/admin/stats">Statistieken</Link></li>
+</ul>
+</nav>
+
+<div className="hamburger">≡</div>
+</header>
+);
+};
+
+export default AdminHeader;
