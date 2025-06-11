fd5954d8c75f9328fbb8ab986db622788d1d2b70
LailaNounouh <lailanounouh@gmail.com>
2025-06-11 15:46:02 +0200

    toevoegen van wijzigingen bij
    payment card en info sectie toe aan StatusBetaling component en CSS

--- programingproject/src/pages/Bedrijven/statusbetaling.jsx
+++ programingproject/src/pages/Bedrijven/statusbetaling.jsx
@@ -5,9 +5,20 @@ const StatusBetaling = () => {
   return (
     <div className="payment-status-page">
       <div className="payment-status">
-        {/* Hier komen straks de payment cards */}
+        {/* payment cards */}
       </div>
+      <div className="payment-card">
+  <div className="payment-info">
+    <span>Factuur F2023-0456</span>
+    {/*status badge */}
+  </div>
+  <div>
+    <span>Bedrag: €1200</span> | <span>Datum: 20-04-2023</span>
+  </div>
+</div>
+
     </div>
+    
   );
 };
 
