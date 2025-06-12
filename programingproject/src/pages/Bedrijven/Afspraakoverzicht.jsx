2b85c835a67d5c57be95cf640d8307adef9f4959
LailaNounouh <lailanounouh@gmail.com>
2025-06-12 15:00:45 +0200

    Maak component klaar voor toekomstige afsprakenlijst
    en toevoegen van conssistente style , routepad om pagina bij klick te laden

--- programingproject/src/pages/Bedrijven/Afspraakoverzicht.jsx
+++ programingproject/src/pages/Bedrijven/Afspraakoverzicht.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './AfspraakOverzicht.css';
 
-const AfspraakOverzicht = () => {
+const AfspraakOverzicht = ({ afspraken = [] }) => {
   return (
-    <div className="afspraak-overzicht">
+    <main className="afspraak-overzicht">
       <header className="afspraak-overzicht__header">
-        <h1>Afspraakoverzicht</h1>
+        <h1 className="afspraak-overzicht__titel">Afspraakoverzicht</h1>
       </header>
-      <div className="afspraak-overzicht__geen-afspraken">
-        <span>Geen afspraken gepland</span>
-      </div>
-    </div>
+      
+      <section className="afspraak-overzicht__content">
+        {afspraken.length > 0 ? (
+          <ul className="afspraak-lijst">
+            {/* Toekomstige afspraken worden hier gerenderd */}
+          </ul>
+        ) : (
+          <p className="afspraak-overzicht__geen-afspraken">
+            Momenteel zijn er geen afspraken gepland.
+          </p>
+        )}
+      </section>
+    </main>
   );
 };
 
