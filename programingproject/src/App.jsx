8479a51c43a228ffb0736683487b07d6f95ececc
LailaNounouh <lailanounouh@gmail.com>
2025-06-12 14:39:46 +0200

    Basisstructuur voor AfspraakOverzicht

--- programingproject/src/App.jsx
+++ programingproject/src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/layout/algemenelayout/Layout";
 import StudentenLayout from "./components/layout/studentenlayout/StudentenLayout";
-import BedrijvenLayout from "./components/layout/bedrijvenlayout/BedrijvenLayout"
+import BedrijvenLayout from "./components/layout/bedrijvenlayout/BedrijvenLayout";
 import AdminLayout from "./components/layout/adminlayout/AdminLayout";
-import SeekerLayout from "./components/layout/seekerlayout/SeekerLayout"
+import SeekerLayout from "./components/layout/seekerlayout/SeekerLayout";
 
 import Home from "./pages/Algemeen/Home";
 import Login from "./pages/Algemeen/login/Login";
@@ -28,21 +28,29 @@ import StandenModule from "./pages/Modules/StandenModule";
 export default function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Layout><Home /></Layout>}/>
-          <Route path="/login" element={<Layout><Login /></Layout>} />
-          <Route path="/register" element={<Layout><Register /></Layout>} />
-          <Route path="/about" element={<Layout><About /></Layout>} />
-          <Route path="/contact" element={<Layout><Contact /></Layout>} />
-          
-          <Route path="/seeker" element={<SeekerLayout><SeekerDashboard /></SeekerLayout>} />
-          <Route path="/student" element={<StudentenLayout><StudentDashboard /></StudentenLayout>} />
-          <Route path="/student/Bedrijven" element={<StudentenLayout><BedrijvenModule/></StudentenLayout>} />
-          <Route path="/admin" element={<AdminLayout><AdminDashboard /></AdminLayout>} />
-          <Route path="/bedrijf" element={<BedrijvenLayout><BedrijvenDashboard /></BedrijvenLayout>} />
-          <Route path="/bedrijf/betaling" element={<BedrijvenLayout><StatusBetaling/></BedrijvenLayout>} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/login" element={<Layout><Login /></Layout>} />
+        <Route path="/register" element={<Layout><Register /></Layout>} />
+        <Route path="/about" element={<Layout><About /></Layout>} />
+        <Route path="/contact" element={<Layout><Contact /></Layout>} />
+
+        <Route path="/seeker" element={<SeekerLayout><SeekerDashboard /></SeekerLayout>} />
+        <Route path="/student" element={<StudentenLayout><StudentDashboard /></StudentenLayout>} />
+        <Route path="/student/bedrijven" element={<StudentenLayout><BedrijvenModule /></StudentenLayout>} />
+
+        <Route path="/admin" element={<AdminLayout><AdminDashboard /></AdminLayout>} />
+
+        <Route path="/bedrijf" element={<BedrijvenLayout><BedrijvenDashboard /></BedrijvenLayout>} />
+        <Route path="/bedrijf/betaling" element={<BedrijvenLayout><StatusBetaling /></BedrijvenLayout>} />
+        <Route path="/bedrijf/afspraken" element={<BedrijvenLayout><AfsprakenModule /></BedrijvenLayout>} />
+
+        
+        <Route path="/admin/gebruikers" element={<AdminLayout><GebruikersModule /></AdminLayout>} />
+        <Route path="/admin/standen" element={<AdminLayout><StandenModule /></AdminLayout>} />
+
+        <Route path="*" element={<Notfound />} />
+      </Routes>
     </Router>
   );
 }
