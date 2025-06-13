8a9a5c4ecb49928354096ac95044e30be3f23519
Nehlito <nehlito@hotmail.com>
2025-06-13 15:08:36 +0200

    Update About

--- programingproject/src/pages/Algemeen/About.jsx
+++ programingproject/src/pages/Algemeen/About.jsx
@@ -3,16 +3,15 @@ import './About.css';
 
 function About() {
   return (
-    <div className="about-content">
+    <div className="about-page">
       
-      <header className="haeder-bar">
-        <div className="header-content">
+       <div className="header-box">
         <h1>Over Career Launch</h1>
         <p className="subtitle">Verbind studenten, werkzoekenden en bedrijven</p>
-      </div>
-      </header>
+       </div>
 
-      <section className="description">
+      <div className="content-box">
+       
         <p>
           <span className="highlight">Career Launch</span> is opgericht met één duidelijk doel: 
           het verbinden van talentvolle studenten en werkzoekenden met bedrijven die hen verder willen helpen. 
@@ -31,7 +30,7 @@ function About() {
         <p>
           Onze missie is helder: <span className="highlight">carrières lanceren</span> door verbindingen te maken die écht het verschil maken.
         </p>
-      </section>
+      </div>
     </div>
   );
 }
