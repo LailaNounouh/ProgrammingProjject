8515da685bf2ab107ee4918b6dcdd49da6010b71
LailaNounouh <lailanounouh@gmail.com>
2025-06-13 17:06:04 +0200

    Basistructuur settingsbedrijf

---
+++ programingproject/src/pages/Bedrijven/Settingsbedrijf.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Settingsbedrijf = () => {
+  return (
+    <div className="instellingen-pagina">
+      <div className="instellingen-container">
+        <div className="instellingen-header">
+          <h1>Instellingen</h1>
+          <p className="instellingen-subtitel">
+            Beheer hier de contactgegevens van uw bedrijf
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settingsbedrijf;
