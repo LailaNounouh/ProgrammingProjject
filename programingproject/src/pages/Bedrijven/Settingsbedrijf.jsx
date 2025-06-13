ed0aeaed26044e761fb7d4aea9889b3fec640366
LailaNounouh <lailanounouh@gmail.com>
2025-06-13 17:09:50 +0200

    terug btn en styling

--- programingproject/src/pages/Bedrijven/Settingsbedrijf.jsx
+++ programingproject/src/pages/Bedrijven/Settingsbedrijf.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
+const navigate = useNavigate();
+
+const handleTerug = () => {
+  navigate("/Bedrijf");
+};
 
 const Settingsbedrijf = () => {
   return (
     <div className="instellingen-pagina">
       <div className="instellingen-container">
+        <button onClick={handleTerug} className="terug-knop">
+  <FiArrowLeft className="terug-icoon" /> Terug naar dashboard
+</button>
         <div className="instellingen-header">
           <h1>Instellingen</h1>
           <p className="instellingen-subtitel">
