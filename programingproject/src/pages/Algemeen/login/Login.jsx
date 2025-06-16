ec30d4136c7ffc778a15293a9baade0e5a93de5d
LailaNounouh <182747544+Lvilv18@users.noreply.github.com>
2025-06-16 20:35:55 +0200

    verwijderen van 1 module van usenavigate voor correcte werking

--- programingproject/src/pages/Algemeen/login/Login.jsx
+++ programingproject/src/pages/Algemeen/login/Login.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { baseUrl } from '../../../config';
 import "./Login.css";
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../context/AuthProvider";
 
 export default function Login() {
