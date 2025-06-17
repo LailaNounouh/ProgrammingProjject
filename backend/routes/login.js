97e90f2b61fe29ed4889f69f6ebe79205bee5813
simonbeelen <74742179+simonbeelen@users.noreply.github.com>
2025-06-17 11:14:46 +0200

    bugfix login

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -28,7 +28,7 @@ router.post('/', async (req, res) => {
 
     const query = `
       SELECT 
-        Student_id AS id, -- Gebruik alias voor consistentie met de frontend
+        student_id AS id, -- Gebruik alias voor consistentie met de frontend
         email,
         wachtwoord,
         naam
