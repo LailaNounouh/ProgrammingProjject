fc5132e88b660a8e09e2330b6865ce018882b7aa
LailaNounouh <182747544+Lvilv18@users.noreply.github.com>
2025-06-18 23:51:07 +0200

    basis sidebar component toegevoegd met navigatie en iconen

---
+++ programingproject/src/pages/Bedrijven/Sidebar/Sidebar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  FaEuroSign,
+  FaCalendarAlt,
+  FaMapMarkerAlt,
+  FaCog,
+} from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
+export const Sidebar = ({ showMobileMenu, setShowMobileMenu }) => {
+  const navigate = useNavigate();
+
+  const menuItems = [
+    {
+      title: 'Staat van betaling',
+      icon: <FaEuroSign />,
+      onClick: () => navigate('/bedrijf/betaling'),
+      color: 'blue'
+    },
+    {
+      title: 'Afspraakoverzicht',
+      icon: <FaCalendarAlt />,
+      onClick: () => navigate('/bedrijf/afspraken'),
+      color: 'green'
+    },
+    {
+      title: 'Beschikbaarheid van standen',
+      icon: <FaMapMarkerAlt />,
+      onClick: () => navigate('/bedrijf/standen'),
+      color: 'orange'
+    },
+    {
+      title: 'Bedrijfsinstellingen',
+      icon: <FaCog />,
+      onClick: () => navigate('/bedrijf/instellingen'),
+      color: 'purple'
+    }
+  ];
+
+  return (
+    <aside className={`sidebar ${showMobileMenu ? 'active' : ''}`}>
+      <h3>Snelmenu</h3>
+      <ul>
+        {menuItems.map((item, index) => (
+          <li
+            key={index}
+            onClick={() => {
+              item.onClick();
+              setShowMobileMenu(false);
+            }}
+          >
+            <span className="icon">{item.icon}</span>
+            <span>{item.title}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
