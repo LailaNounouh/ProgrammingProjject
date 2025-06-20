b79a06c3e320bb3a6e9bf5faa7faca3ad880fab9
Nehlito <nehlito@hotmail.com>
2025-06-20 13:20:12 +0200

    witte ruimte onder header verwijderd door marges in header.css aan te passen

--- programingproject/src/components/layout/algemenelayout/Footer.jsx
+++ programingproject/src/components/layout/algemenelayout/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Carreerlaunch. All rights reserved.</p>
+      <p>&copy; 2026 Carreerlaunch. All rights reserved.</p>
     </footer>
   );
 };
