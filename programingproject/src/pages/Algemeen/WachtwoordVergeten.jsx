0ff2a106671fdd376f66e209fbf1ea958c10c12b
Nehlito <nehlito@hotmail.com>
2025-06-20 12:51:40 +0200

    WachtwoordVergeten.jsx aangepast en aparte CSS toegevoegd

--- programingproject/src/pages/Algemeen/WachtwoordVergeten.jsx
+++ programingproject/src/pages/Algemeen/WachtwoordVergeten.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import "./WachtwoordVergeten.css";
+
+
 
 function WachtwoordVergeten() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Wachtwoord vergeten</h1>
-      <p>Vul je e-mailadres in om een resetlink te ontvangen.</p>
-      <form>
-        <input type="email" placeholder="E-mailadres" required />
-        <button type="submit">Verstuur</button>
-      </form>
+     <div className="wachtwoordvergeten-container">
+      <div className="wachtwoordvergeten-card">
+        <h1>Wachtwoord vergeten</h1>
+        <p>Vul je e-mailadres in om een resetlink te ontvangen.</p>
+        <form>
+          <input type="email" placeholder="E-mailadres" required />
+          <button type="submit">Verstuur</button>
+        </form>
+        <div className="wachtwoordvergeten-terug-link">
+          <Link to="/login">Terug naar inloggen</Link>
+        </div>
+      </div>
     </div>
   );
 }
