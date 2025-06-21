7c129fc02a88e09f9571e67bf2e9a5770f543618
Bjorn-guiot <bjorn.guiot@student.ehb.be>
2025-06-21 23:20:33 +0200

    baxkend cors problem
    baxkend cors problem solving

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,10 @@ const io = socketIo(server, {
 });
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:4173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
