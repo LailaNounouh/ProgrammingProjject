bdac3665187cdd050c0319c7f54a818cd26900df
Nehlito <nehlito@hotmail.com>
2025-06-22 13:42:05 +0200

    Header.jsx
    Verwijder navigatie voor ingelogde gebruikers uit de homepagina-header

--- programingproject/src/components/layout/algemenelayout/Header.jsx
+++ programingproject/src/components/layout/algemenelayout/Header.jsx
@@ -43,17 +43,8 @@ const Header = () => {
           <li><Link to="/" onClick={closeMobileMenu}>Home</Link></li>
           <li><Link to="/about" onClick={closeMobileMenu}>About</Link></li>
           <li><Link to="/contact" onClick={closeMobileMenu}>Contact</Link></li>
-
-
-          {user ? (
-            <>
-              <li><Link to={`/${user.role}`}>Dashboard</Link></li>
-              <li><Link to={`/${user.role}/profiel`}>Account</Link></li>
-              <li><LogoutButton /></li>
-            </>
-          ) : (
-            <li><Link to="/login" onClick={closeMobileMenu}>Login / Registreren</Link></li>
-          )}
+          <li><Link to="/login" onClick={closeMobileMenu}>Login / Registreren</Link></li>
+        
         </ul>
       </nav>
     </header>
