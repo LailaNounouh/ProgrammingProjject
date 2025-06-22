1556684dd1481c252eb69bae0183bf268f0cdfc5
Nehlito <nehlito@hotmail.com>
2025-06-22 16:06:23 +0200

    Studentenheader aangepast

--- programingproject/src/components/layout/studentenlayout/StudentenHeader.jsx
+++ programingproject/src/components/layout/studentenlayout/StudentenHeader.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import LogoutButton from '../../button/logoutbutton'; 
+import { useAuth } from '../../../context/AuthProvider';  
 import './StudentenHeader.css';
 
-const userType = localStorage.getItem('userType')
 
 const StudentenHeader = () => {
+  const { uitloggen } = useAuth();
+  const [showLogoutPopup, setShowLogoutPopup] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
  return (
-   <header className="header">
+   <>
+   <header className="student-header">
      <Link to="/" className="logo-link">
        <img
          src={`${import.meta.env.BASE_URL}afbeelding/erasmuslogo.png`}
@@ -15,18 +19,44 @@ const StudentenHeader = () => {
          className="logo"
        />
      </Link>
-     <nav>
+
+     <button
+          className="hamburger"
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+        >
+          ☰
+     </button>
+
+     <nav className={showMobileMenu ? 'show' : ''}>
        <ul>
          <li><Link to="/student">Dashboard</Link></li>
          <li><Link to="/student/bedrijven">Deelnemende Bedrijven</Link></li>
          <li><Link to="/student/standen">Standen</Link></li>
          <li><Link to="/student/afspraken">Afspraak maken</Link></li>
          <li><Link to="/student/account">Account</Link></li>
-         <li><LogoutButton /></li>
+         <li>
+            <button
+                onClick={() => setShowLogoutPopup(true)}
+                className="logout-link"
+            >
+                Uitloggen
+            </button>
+         </li>
        </ul>
      </nav>
    </header>
- );
+
+  {showLogoutPopup && (
+        <div className="logout-popup-overlay">
+          <div className="logout-popup">
+            <p>Weet je zeker dat je wilt uitloggen?</p>
+            <button onClick={uitloggen} className="confirm-button">Bevestig uitloggen</button>
+            <button onClick={() => setShowLogoutPopup(false)} className="cancel-button">Annuleer</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default StudentenHeader;
